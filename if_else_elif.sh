bff8200f46bb871a0a8ff985ad3b5ec959d966dc
anabsyaz <anabsyaz@gmail.com>
2026-04-14 10:35:35 -0500

    Added an explanation of all statements and common mistakes people make when using the statements

--- if_else_elif.sh
+++ if_else_elif.sh
@@ -11,3 +11,10 @@ elif [ $number -lt 0 ]; then
 else
   echo "The number is zero."
 fi
+# The if statement allows us to execute a block of code if a certain condition is true.
+# The elif statement allows us to check multiple conditions in sequence.
+# The else statement allows us to execute a block of code if none of the previous conditions were true.
+# Common mistakes when using if, else, and elif statements:
+# 1. Forgetting to include the "then" keyword after the condition
+# 2. Not using the correct syntax for the condition
+# 3. Not using "fi" to end the if statement
