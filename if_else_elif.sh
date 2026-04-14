c309dc1e1fb71f64a953ace564f84dc12735f303
anabsyaz <anabsyaz@gmail.com>
2026-04-14 10:30:11 -0500

    Made an example where the if/elif/else statement is being used.

--- if_else_elif.sh
+++ if_else_elif.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # This script demonstrates the use of if, else, and elif statements in bash.
+
+echo "Enter a number:"
+read number
+if [ $number -gt 0 ]; then
+  echo "The number is positive."
+elif [ $number -lt 0 ]; then
+  echo "The number is negative."
+else
+  echo "The number is zero."
+fi
