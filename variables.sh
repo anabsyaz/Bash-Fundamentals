5ed8c75b1ae207e038bf268bb3ac13427e14f72c
anabsyaz <anabsyaz@gmail.com>
2026-04-16 10:10:07 -0500

    Added common variable types

--- variables.sh
+++ variables.sh
@@ -12,3 +12,12 @@ name="John"
 # For example, to print the value of the "name" variable, you would write:
 echo $name
 # This will output "John" to the terminal.
+# Variables are case-sensitive, which means that "name" and "Name" are considered different variables.
+# Variables are useful for storing data that can be used later in the script, such as user input, file names, or any other data that you want to use multiple times.
+#
+# Common variable types include:
+# - String: A sequence of characters, such as "Hello, World!" and are enclosed in double quotes.
+# - Integer: A whole number, such as 42 or -10.
+# - Float: A number with a decimal point, such as 3.14 or -0.5.
+# - Boolean: A value that can be either true or false, such as true or false.
+# - Array: A collection of values, such as (1 2 3) or ("apple" "banana" "cherry").
