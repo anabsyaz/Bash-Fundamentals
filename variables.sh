b6e166442c437dad7193396418283395e9582f1d
anabsyaz <anabsyaz@gmail.com>
2026-04-16 10:02:54 -0500

    Added an explanation of how variables for and how to use them

--- variables.sh
+++ variables.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# This file will show how variables work in bash. 
+# Variables are used to store data that can be used later in the script.
+# To create a variable, you simply need to assign a value to it.
+# The syntax for creating a variable is:
+# variable_name=value
+# There should be no spaces around the equal sign.
+# For example, to create a variable called "name" and assign it the value "John", you would write:
+name="John"
+# To access the value of a variable, you need to use the dollar sign ($) before the variable name.
+# For example, to print the value of the "name" variable, you would write:
+echo $name
+# This will output "John" to the terminal.
