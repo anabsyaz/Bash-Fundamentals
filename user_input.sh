40423e32410f8898ad43afb89c996223250f7133
anabsyaz <anabsyaz@gmail.com>
2026-04-20 10:10:10 -0500

    Added an example as well as teaches how to read user input.

--- user_input.sh
+++ user_input.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# This script shows how to read user input in bash, common issues people have with reading input, and an example of reading input from the user.
+# To read input from the user, we can use the "read" command. The "read" command takes a variable name as an argument and stores the user input in that variable.
+echo "Please enter your name:"
+read name
+echo "Hello, $name! Welcome to the world of bash scripting."
