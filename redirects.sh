4f6d6491d01e6615159a9bdd5e10b9bf62f928ca
anabsyaz <anabsyaz@gmail.com>
2026-05-04 10:02:58 -0500

    Added an explanation of what redirects do and what they are useful for.

--- redirects.sh
+++ redirects.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+# This script teaches how to use redirects in Bash, including input and output redirection, as well as appending output to files.
+# Redirection allows you to change the source of input or the destination of output for commands.
+# They are useful for saving command output to files, reading input from files, and chaining commands together.
+
