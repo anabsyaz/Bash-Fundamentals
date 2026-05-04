ea1eb9da57068628852c8b34f957adcccdabd52e
anabsyaz <anabsyaz@gmail.com>
2026-05-04 10:03:44 -0500

    Added common operators when redirecting.

--- redirects.sh
+++ redirects.sh
@@ -4,3 +4,7 @@
 # Redirection allows you to change the source of input or the destination of output for commands.
 # They are useful for saving command output to files, reading input from files, and chaining commands together.
 
+# Common redirection operators:
+# > : Redirects standard output to a file, overwriting the file if it already exists
+# >> : Redirects standard output to a file, appending to the file if it already exists
+# < : Redirects standard input from a file
