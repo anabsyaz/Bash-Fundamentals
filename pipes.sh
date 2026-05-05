379de47afd559f57d0537da654f36e09d98d90e9
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-05 10:06:35 -0500

    Added an explanation as to what are pipes and how they are useful.

--- pipes.sh
+++ pipes.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# This script teaches how to use pipes in Bash, including how to chain commands together and redirect output between them.
+# Pipes allow you to take the output of one command and use it as the input for another command, creating a powerful way to combine commands and process data more efficiently.
+# The pipe operator is represented this | and is used to connect the output of one command to the input of another command.
