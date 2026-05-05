8dd24711d7b3a7ecd38189b8a5627ac6ef3dce8a
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-05 09:58:01 -0500

    Added ways to troubleshoot common mistakes.

--- redirects.sh
+++ redirects.sh
@@ -6,7 +6,7 @@
 
 # Common redirection operators:
 # > : Redirects standard output to a file, overwriting the file if it already exists
-# >> : Redirects standard output to a file, appending to the file if it already exists
+# >> : Redirects standard output to a file, appending to the file if it already exists/adds to the end of the file.
 # < : Redirects standard input from a file
 
 # Example of output redirection:
@@ -17,3 +17,8 @@ echo "This is the new output" > output.txt # This will create or overwrite outpu
 # 2. Forgetting to specify a file for redirection, which can lead to errors or unexpected behavior.
 # 3. Not having the necessary permissions to write to the file, which can cause the redirection to fail.
 # 4. Overwriting important files by using > without realizing it.
+
+# How to troubleshoot the common mistakes:
+# 1. If the output is not what you expected, check the redirection operator to ensure you are using the correct one for your intended purpose (> for overwriting, >> for appending/adding to the end of the file).
+# 2. If you encounter an error about missing files, make sure to specify the correct file name in your redirection command.
+# 3. If you get a permission error, check the file permissions and ensure you have the necessary rights to write to the file, you can do this by using the ls -l command to check the permissions of the file and the chmod command to change permissions if needed.
