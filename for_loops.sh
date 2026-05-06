d5b2e8c35c3b5477644146a5198aca0705beffca
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-06 10:00:13 -0500

    Added 2 examples of for loops being used including brace expansion and just a simple loop iterating numbers.

--- for_loops.sh
+++ for_loops.sh
@@ -4,3 +4,18 @@
 # For loops allow you to iterate over a list of items and execute a block of code for each item.
 # In this example, we will iterate over a list of numbers and print each number to the console.
 # For loops are useful for performing repetitive tasks without having to write the same code multiple times.
+
+# Example of a for loop iterating over a list of numbers
+for i in 1 2 3 4 5; do
+    echo "Number: $i"
+done
+# This will output:
+# Number: 1
+# Number: 2
+# and so on
+
+# We can also use a for loop to iterate over a range of numbers using brace expansion
+for i in {1..5}; do
+    echo "Number: $i"
+done
+# This will produce the same output as the previous example.
