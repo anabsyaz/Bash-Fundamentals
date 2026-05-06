4c2aa7261800230d4458f35ce57bb540aaca69ce
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-06 09:58:17 -0500

    Iterated why for loops may be useful

--- for_loops.sh
+++ for_loops.sh
@@ -3,3 +3,4 @@
 # This script demonstrates the use of for loops in bash.
 # For loops allow you to iterate over a list of items and execute a block of code for each item.
 # In this example, we will iterate over a list of numbers and print each number to the console.
+# For loops are useful for performing repetitive tasks without having to write the same code multiple times.
