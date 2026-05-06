9f22001873bd27e117624da70ee51651b385df21
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-06 09:57:40 -0500

    Added an explanation as to what are for loops

--- for_loops.sh
+++ for_loops.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# This script demonstrates the use of for loops in bash.
+# For loops allow you to iterate over a list of items and execute a block of code for each item.
+# In this example, we will iterate over a list of numbers and print each number to the console.
