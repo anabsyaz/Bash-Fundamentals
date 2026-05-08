50fdf07cabb41f63e71c2bf41321050bbb3c4094
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-08 10:47:33 -0500

    Made an example using a do while loop.

--- do_while_while_loops.sh
+++ do_while_while_loops.sh
@@ -3,3 +3,14 @@
 # This script demonstrates the use of while and do-while loops in bash.
 # While loops allow you to execute a block of code repeatedly as long as a specified condition is true.
 # Do-while loops are similar to while loops, but they execute the block of code at least once before checking the condition.
+
+# Example of a while loop that counts from 1 to 5
+counter=1
+while [ $counter -le 5 ]; do
+    echo "Counter: $counter"
+    ((counter++)) # Increment the counter
+done
+# This will output:
+# Counter: 1
+# Counter: 2
+# and so on
