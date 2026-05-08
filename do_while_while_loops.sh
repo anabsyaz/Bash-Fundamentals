e8d44d37b57e0060e83642a5a590afb277d7f473
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-08 10:44:40 -0500

    Added an explanation to what while and do while loops do.

--- do_while_while_loops.sh
+++ do_while_while_loops.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# This script demonstrates the use of while and do-while loops in bash.
+# While loops allow you to execute a block of code repeatedly as long as a specified condition is true.
+# Do-while loops are similar to while loops, but they execute the block of code at least once before checking the condition.
