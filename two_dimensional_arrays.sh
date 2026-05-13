0a6c3b40b534e573e36c6daa2b52ee0e2b3bb84b
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:19:49 -0500

    Explained the script

--- two_dimensional_arrays.sh
+++ two_dimensional_arrays.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# This script demonstrates how two-dimensional arrays work.
+# Two-dimensional arrays aren't natively supported in bash.
+# We will logically associate two arrays together 
+
