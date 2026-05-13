0ab75080838f0af7d94349b0b2e75ab55181f9dc
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:10:31 -0500

    Added another example of setting a default value that is already set

--- default_values.sh
+++ default_values.sh
@@ -8,4 +8,11 @@
 name=${name:-"John Doe"}
 echo "Name: $name"
 # If the variable 'name' is not set, it will output:
-# Name: John Doe   
+# Name: John Doe
+
+# Example of setting a default value for a variable that is already set
+name="Alice"
+name=${name:-"John Doe"}
+echo "Name: $name"
+# Since 'name' is already set to "Alice", it will output:
+# Name: Alice
