f357ce4f11be0ad94a983eb96bf437629456cbe4
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:05:41 -0500

    Added an explanation to default values

--- default_values.sh
+++ default_values.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# This script demonstrates how to set and use default values for variables in bash.
+# Default values can be assigned to variables using the syntax: variable=${variable:-default_value}.
+# This means that if the variable is not set or is empty, it will take the value of default_value.
