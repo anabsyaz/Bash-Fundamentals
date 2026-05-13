482ec54e402f8a340f22ec33f2bcfd89b72fb61e
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:13:47 -0500

    Added a way to troubleshoot the common issues.

--- default_values.sh
+++ default_values.sh
@@ -27,3 +27,8 @@ greet() {
 # 1. If you forget to use the correct syntax for default values, it may not work as expected.
 # 2. If you set a variable to an empty string, it will be considered as set, and the default value will not be used.
 # 3. If you want to use a default value only when the variable is unset (not set at all), you can
+
+# How to troubleshoot issues with default values:
+# 1. Check if the variable is set or not using the 'set' command or by printing the variable before using it.
+# 2. Make sure you are using the correct syntax for default values.
+# 3. If you want to check if a variable is empty, you can use the syntax: variable=${variable:-default_value} and then check if the variable is empty using an if statement.
