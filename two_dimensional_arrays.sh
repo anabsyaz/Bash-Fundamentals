9a29fbf007f5e53ba4d06ee0547af4e9e1a3b29d
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:22:23 -0500

    explained what is a 2d array

--- two_dimensional_arrays.sh
+++ two_dimensional_arrays.sh
@@ -4,3 +4,9 @@
 # Two-dimensional arrays aren't natively supported in bash.
 # We will logically associate two arrays together 
 
+# A 2D array is essentially an array of arrays.
+# It is used to store data in a more structured way, similar to a table or matrix.
+# Each element of the main array can be another array, allowing us to create a grid-like structure.
+# For example, we can create a 2D array to represent a simple table of data
+# where each row is an array of values.
+
