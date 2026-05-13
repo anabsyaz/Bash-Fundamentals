8a2026f5dd94a46b5d94c4e7c29a721b4ec4a691
anabsyaz <42751652+anabsyaz@users.noreply.github.com>
2026-05-13 10:09:38 -0500

    Added an example of assigning a default value

--- default_values.sh
+++ default_values.sh
@@ -3,3 +3,9 @@
 # This script demonstrates how to set and use default values for variables in bash.
 # Default values can be assigned to variables using the syntax: variable=${variable:-default_value}.
 # This means that if the variable is not set or is empty, it will take the value of default_value.
+
+# Example of setting a default value for a variable
+name=${name:-"John Doe"}
+echo "Name: $name"
+# If the variable 'name' is not set, it will output:
+# Name: John Doe   
